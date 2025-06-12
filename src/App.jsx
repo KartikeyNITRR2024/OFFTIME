1d2bc9e0fe3ed06d610a6364fc149cefc0f5c443
Kartikey2001nitrr <kartikeythawait123@gmail.com>
2025-06-13 01:44:57 +0530

    Add static.json for SPA routing on Render

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/:code" element={<Home />} />
-        <Route path="*" element={<Landing />} />
+        <Route path="/*" element={<Landing />} />
       </Routes>
       <ToastContainer position="top-right" autoClose={1000} />
     </>
